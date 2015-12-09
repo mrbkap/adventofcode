6f29e99bf45f0d37f144e1bb1e43f561bfe8a740
Blake Kaplan <mrbkap@gmail.com>
2015-12-09 09:39:35 -0500

    December 8 second star

--- 8_characters/src/main.rs
+++ 8_characters/src/main.rs
@@ -4,39 +4,29 @@ fn main() {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
 
-    let mut count : usize = 0;
-    let mut chars : usize = 0;
+    let mut unencoded : usize = 0;
+    let mut reencoded : usize = 0;
     for string in buffer.split('\n') {
         let trimmed = string.trim().to_string();
         if trimmed.len() == 0 {
             continue;
         }
 
-        count += 2;
-        let contents = &trimmed[1..trimmed.len() - 1];
-        let mut iter = contents.chars();
+        reencoded += 2;
+        let mut iter = trimmed.chars();
         while let Some(c) = iter.next() {
-            count += 1;
+            unencoded += 1;
             match c {
-                '\\' => {
-                    match iter.nth(0) {
-                        Some('\\') | Some('"') => {
-                            count += 1;
-                            chars += 1;
-                        }
-                        Some('x') => {
-                            match iter.nth(1) {
-                                Some(_) => { count += 3; chars += 1; }
-                                None => { panic!(); }
-                            }
-                        }
-                        _ => { panic!("bad escape sequence {}", c); }
-                    }
+                '"' => {
+                    reencoded += 2; // \"
                 }
-                _ => { chars += 1; }
+                '\\' => {
+                    reencoded += 2; // \\
+                }
+                _ => { reencoded += 1 }
             }
         }
     }
 
-    println!("Count: {} chars: {} diff: {}", count, chars, count - chars);
+    println!("Count: {} chars: {} diff: {}", unencoded, reencoded, reencoded - unencoded);
 }
