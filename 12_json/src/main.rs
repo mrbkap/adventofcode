de68c95bd2b5082200cd1293486c7b54b5c8ee74
Blake Kaplan <mrbkap@gmail.com>
2015-12-12 22:58:11 -0500

    First star December 12

---
+++ 12_json/src/main.rs
@@ -0,0 +1,28 @@
+extern crate rustc_serialize;
+
+use std::io::{self, Read};
+use rustc_serialize::json::Json;
+
+fn find_nums(data: &Json) -> i64 {
+    match *data {
+        Json::I64(i) => { return i; }
+        Json::U64(u) => { return u as i64; }
+        Json::F64(_) => { panic!(); }
+        Json::Array(ref a) => {
+            return a.iter().fold(0i64, |acc, v| acc + find_nums(&v));
+        }
+        Json::Object(ref o) => {
+            return o.values().fold(0i64, |acc, v| acc + find_nums(&v));
+        }
+        Json::String(_) | Json::Boolean(_) | Json::Null => { return 0; }
+    }
+}
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+
+    let data = Json::from_str(&buffer).unwrap();
+    let total = find_nums(&data);
+    println!("total = {}", total);
+}
