372c9000a9a0829e006905d121fe62369f7666cf
Blake Kaplan <mrbkap@gmail.com>
2015-12-21 23:26:36 -0800

    December 19 second star

--- 19_reindeer_medicine/src/main.rs
+++ 19_reindeer_medicine/src/main.rs
@@ -1,11 +1,32 @@
 use std::io;
 use std::io::BufRead;
-use std::collections::{HashMap, HashSet};
+
+struct Rep {
+    left: String,
+    right: String,
+}
+
+fn try(m: &str, d: u32, c: usize, rs: &Vec<Rep>) -> Option<u32> {
+    if m == "e" {
+        return Some(d);
+    }
+
+    for r in rs.iter() {
+        if let Some(idx) = m.find(&r.right) {
+            let mm = m[0..idx].to_string() + &r.left + &m[(idx + r.right.len())..];
+            if let Some(depth) = try(&mm, d + 1, c + 1, rs) {
+                return Some(depth);
+            }
+        }
+    }
+
+    return None;
+}
 
 fn main() {
     let stdin = io::stdin();
 
-    let mut replacements: HashMap<String, Vec<String>> = HashMap::new();
+    let mut replacements: Vec<Rep> = Vec::new();
     let mut lines_iter = stdin.lock().lines();
     loop {
         let line = lines_iter.next().unwrap().unwrap();
@@ -15,22 +36,16 @@ fn main() {
         }
 
         let words: Vec<_> = trimmed.split(" => ").collect();
-        let looked_up = replacements.entry(words[0].to_string()).or_insert(Vec::new());
-        looked_up.push(words[1].to_string());
+        let rep = Rep { left: words[0].to_string(), right: words[1].to_string(), };
+        match replacements.iter().position(|r| { r.right.len() <= words[1].len() }) {
+            Some(idx) => { replacements.insert(idx, rep); }
+            None => { replacements.push(rep); }
+        }
     }
 
     let raw_molecule = lines_iter.next().unwrap().unwrap();
     let molecule = raw_molecule.trim();
 
-    let mut transformed = HashSet::new();
-    for (key, reps) in &replacements {
-        for (i, _) in molecule.match_indices(key) {
-            for rep in reps {
-                let mutated = molecule[0..i].to_string() + rep + &molecule[(i + key.len())..];
-                transformed.insert(mutated);
-            }
-        }
-    }
-
-    println!("there were {}", transformed.len());
+    let idx = try(molecule, 0, 0, &replacements);
+    println!("{:?}", idx);
 }
