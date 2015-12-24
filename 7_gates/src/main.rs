d66fb2b2952bdde08d1147a04b9d92412f544d64
Blake Kaplan <mrbkap@gmail.com>
2015-12-24 11:52:39 -0800

    Make the Rust version of this work.

--- 7_gates/src/main.rs
+++ 7_gates/src/main.rs
@@ -3,12 +3,19 @@ use std::io::BufRead;
 use std::collections::{HashMap, HashSet};
 
 #[derive(Clone)]
+enum Operand {
+    Const(u16),
+    Gate(String),
+}
+
+#[derive(Clone)]
 enum Value {
-    And(String, String),
-    Or(String, String),
-    Lshift(String, u16),
-    Rshift(String, u16),
-    Not(String),
+    And(Operand, Operand),
+    Or(Operand, Operand),
+    Lshift(Operand, u16),
+    Rshift(Operand, u16),
+    Not(Operand),
+    Move(Operand),
 }
 
 #[derive(Clone)]
@@ -26,42 +33,82 @@ impl <'a> GateGame {
         return GateGame { gates: HashMap::new() };
     }
 
+    fn parse_operand(word: &str) -> Operand {
+        match word.chars().next().unwrap() {
+            '0'...'9' => {
+                Operand::Const(word.parse::<u16>().unwrap())
+            }
+            _ => {
+                Operand::Gate(word.to_string())
+            }
+        }
+    }
+
     pub fn handle_line(self: &'a mut GateGame, l: &str) {
         let v : Vec<&str> = l.split_whitespace().collect();
 
-        match v[0].chars().next().unwrap() {
-            'N' => {
-                assert!(v.len() == 4);
-                self.gates.insert(v.last().unwrap().to_string(), MaybeResolved::Value(Value::Not(v[1].to_string())));
-            }
-            '0'...'9' => {
-                assert!(v.len() == 3);
-                self.gates.insert(v.last().unwrap().to_string(), MaybeResolved::Resolved(v[0].parse::<u16>().unwrap()));
-            }
-            _  => {
-                assert!(v.len() == 5);
-                let value = match v[1] {
-                    "AND" => { Value::And(v[0].to_string(), v[2].to_string()) }
-                    "OR" => { Value::Or(v[0].to_string(), v[2].to_string()) }
-                    "LSHIFT" => { Value::Lshift(v[0].to_string(), v[2].parse::<u16>().unwrap()) }
-                    "RSHIFT" => { Value::Rshift(v[0].to_string(), v[2].parse::<u16>().unwrap()) }
-                    _ => { panic!(); }
-                };
+        let target = v.last().unwrap().to_string();
 
-                self.gates.insert(v.last().unwrap().to_string(), MaybeResolved::Value(value));
-            }
+        // Only prefix operator.
+        if v[0] == "NOT" {
+            assert!(v.len() == 4);
+            let rhs = GateGame::parse_operand(v[1]);
+            self.gates.insert(target, MaybeResolved::Value(Value::Not(rhs)));
+            return;
         }
+
+        let lhs = GateGame::parse_operand(v[0]);
+        // Move is the only unary operator.
+        if v[1] == "->" {
+            self.gates.insert(target, MaybeResolved::Value(Value::Move(lhs)));
+            return;
+        }
+
+        let rhs = GateGame::parse_operand(v[2]);
+        let value = match v[1] {
+            "AND" => { Value::And(lhs, rhs) }
+            "OR" => { Value::Or(lhs, rhs) }
+            "LSHIFT" => {
+                // Must shift by constant numbers.
+                if let Operand::Const(shift) = rhs {
+                    Value::Lshift(lhs, shift)
+                } else {
+                    panic!("bad left shift");
+                }
+            }
+            "RSHIFT" => {
+                // Must shift by constant numbers.
+                if let Operand::Const(shift) = rhs {
+                    Value::Rshift(lhs, shift)
+                } else {
+                    panic!("bad right shift");
+                }
+            }
+            _ => { panic!(); }
+        };
+
+        self.gates.insert(target, MaybeResolved::Value(value));
     }
 
     pub fn resolve(&mut self) {
         let mut resolving = HashSet::new();
         let v: Vec<_> = self.gates.keys().cloned().collect();
         for key in v {
-            self._resolve(&key, &mut resolving);
+            self._resolve(&Operand::Gate(key), &mut resolving);
         }
     }
 
-    fn _resolve(self: &'a mut GateGame, gname: &str, resolving: &mut HashSet<String>) -> u16 {
+    pub fn resolve_gate(&mut self, gate: &str) -> u16 {
+        let mut resolving = HashSet::new();
+        self._resolve(&Operand::Gate(gate.to_string()), &mut resolving)
+    }
+
+    fn _resolve(self: &'a mut GateGame, o: &Operand, resolving: &mut HashSet<String>) -> u16 {
+        if let &Operand::Const(c) = o {
+            return c;
+        }
+
+        let gname: &str = if let &Operand::Gate(ref g) = o { g } else { panic!() };
         if resolving.contains(gname) {
             panic!("recursive lookup");
         }
@@ -76,6 +123,10 @@ impl <'a> GateGame {
             MaybeResolved::Value(Value::Not(ref s)) => {
                 let rhs = self._resolve(s, resolving);
                 !rhs
+            }
+            MaybeResolved::Value(Value::Move(ref s)) => {
+                let rhs = self._resolve(s, resolving);
+                rhs
             }
             MaybeResolved::Value(Value::And(ref lhs, ref rhs)) => {
                 let lhsv = self._resolve(lhs, resolving);
@@ -116,14 +167,22 @@ fn main() {
         game.handle_line(line.unwrap().trim());
     }
 
-    game.resolve();
+    // Part two.
+    game.gates.insert("b".to_string(), MaybeResolved::Resolved(3176u16));
 
+    // Not strictly necessary, but nice to do.
+    game.resolve();
     for (gatename, value) in game.iter() {
-        if let MaybeResolved::Resolved(v) = *value {
-            println!("{} is {}", gatename, v);
-        }
-        else {
-            panic!();
+        match value {
+            &MaybeResolved::Resolved(v) => {
+                println!("{} is {}", gatename, v);
+            }
+            _ => {
+                panic!();
+            }
         }
     }
+
+    // OK all on its own.
+    println!("a is {}", game.resolve_gate("a"));
 }
